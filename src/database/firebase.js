167c7557229c40a903c56bd986d86d1056fc808c
Kanghj <rkdguswns0803@gmail.com>
2022-12-16 12:45:40 +0900

    firebase정보 .env 적용

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbPdoVP-63Sk1vK9gkBIChuT3V5b-epZk",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: "blog-c1a2e.firebaseapp.com",
   projectId: "blog-c1a2e",
   storageBucket: "blog-c1a2e.appspot.com",
   messagingSenderId: "758927820056",
-  appId: "1:758927820056:web:be43807a986acac5690a6d",
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
