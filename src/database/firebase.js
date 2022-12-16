965107145f88b103ec1981d1a6ffca8217e8f69e
Kanghj <rkdguswns0803@gmail.com>
2022-12-16 10:44:40 +0900

    firebase 연결

---
+++ src/database/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbPdoVP-63Sk1vK9gkBIChuT3V5b-epZk",
+  authDomain: "blog-c1a2e.firebaseapp.com",
+  projectId: "blog-c1a2e",
+  storageBucket: "blog-c1a2e.appspot.com",
+  messagingSenderId: "758927820056",
+  appId: "1:758927820056:web:be43807a986acac5690a6d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter } from "react-router-dom";
 
+// 웹을 사용할 때 firebase를 들고오기 위해 import
+// import {} from, import from을 통해 가져오는 경우
+// -> export, export default로 되어있는 값을 가져와
+//    현재 js 파일에서만 사용 (다른 곳에서 사용하려면 다시 import)
+// js, css를 들고 올때 import하여 들고오는 경우
+// -> 전체 파일에 그 내용이 적용
+import "./database/firebase";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
