9edccf128ca68cdd4897f10401ed93d003a4fdf7
Kanghj <rkdguswns0803@gmail.com>
2022-12-16 11:49:09 +0900

    firebase를 이용한 회원가입

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const LoginForm = () => {
+  // page를 이동하기 위한 navigate
+  const navigate = useNavigate();
+
+  // 값을 가져올 email과 password를 가져올 state
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // 이메일로 회원가입 하기위한 함수
+  const emailCreate = () => {
+    // getAuth는 firebase앱에서 인증 부분을 받아오는 함수
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 회원가입 성공
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+        if (errorCode === "auth/email-already-in-use") {
+          alert("이미 사용중인 E-mail입니다.");
+        } else if (password.length < 6) {
+          alert("password는 6자리 이상으로 설정하세요.");
+        }
+      });
+  };
+
   return (
     <div>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -15,13 +56,19 @@ const LoginForm = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Log In
         </Button>
       </Form>
-      <Button>Sign Up</Button>
+      <Button onClick={emailCreate}>Sign Up</Button>
       <Button>Google Log In</Button>
     </div>
   );
